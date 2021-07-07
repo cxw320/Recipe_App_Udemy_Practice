14da984157b31184bf4ac37aa716c6859eaaccf0
Caroline Wang <cxw320@case.edu>
2021-07-07 16:08:24 -0400

    Update DiffUtil

--- app/src/main/java/com/example/udemyfoodapp/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/example/udemyfoodapp/adapters/RecipesAdapter.kt
@@ -1,21 +1,58 @@
 package com.example.udemyfoodapp.adapters
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.udemyfoodapp.RecipesRowlayoutBinding
+import com.example.udemyfoodapp.models.FoodRecipe
+import com.example.udemyfoodapp.models.Result
+import com.example.udemyfoodapp.util.RecipesDiffUtil
+import android.view.LayoutInflater
+import android.view.ViewGroup
 
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
-    class MyViewHolder: RecyclerView.ViewHolder(){
 
+
+    private var recipes = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+            fun bind(result: Result){
+                binding.result = result
+                binding.executePendingBindings()
+            }
+
+            companion object {
+                fun from(parent: ViewGroup): MyViewHolder {
+                    val layoutInflater = LayoutInflater.from(parent.context)
+                    val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                    return MyViewHolder(binding)
+                }
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int){
-
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
     }
 
     override fun getItemCount(): Int{
+        return recipes.size
+    }
 
+    //passing data to recycler view to show new data
+    // one paramter: FoodRecipe, it will update current list
+    //DiffUtil will compare
+
+    fun setData(newData:FoodRecipe){
+        val recipesDiffUtil =
+            RecipesDiffUtil(recipes, newData.results) //pass in old list (recipes) and the new list (newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipes = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
     }
 
 }
---
+++ app/src/main/java/com/example/udemyfoodapp/util/RecipesDiffUtil.kt
@@ -0,0 +1,26 @@
+package com.example.udemyfoodapp.util
+
+class RecipesDiffUtil(
+    private val oldList: List<Result>,
+    private val newList: List<Result>
+) : DiffUtil.Callback {
+
+    override fun getOldListSize(): Int{
+        return oldList.size
+    } // returns the size of the old list
+
+    override fun getNewListSize(): Int{
+        return newList.size
+    } // returns the size of the new list
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition:Int): Boolean{
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    } //Called by the DiffUtil to decide whether 2 objects represent the same item in the old and new list
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean{
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    } //checks if 2 items have the same data
+
+
+
+}
