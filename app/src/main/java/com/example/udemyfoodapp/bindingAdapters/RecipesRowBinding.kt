26ae785f44af1039139f68aca43908725838b54b
Caroline Wang <cxw320@case.edu>
2021-07-09 14:11:27 -0400

    Update Data Binding

---
+++ app/src/main/java/com/example/udemyfoodapp/bindingAdapters/RecipesRowBinding.kt
@@ -0,0 +1,67 @@
+package com.example.udemyfoodapp.bindingAdapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import coil.load
+import com.example.udemyfoodapp.R
+
+class RecipesRowBinding {
+
+    companion object {
+
+        @BindingAdapter("loadImageUrl")
+        @JvmStatic
+        fun loadImageFromUrl(imageView: ImageView, imageUrl: String){
+            imageView.load(imageUrl){
+                crossfade(600)
+            }
+        }
+
+
+
+        @BindingAdapter("setNumberOfLikes")
+        @JvmStatic
+        fun setNumberOfLikes(textView: TextView, likes: Int){
+            textView.text = likes.toString()
+        //converts the likes from int to string
+        }
+
+
+        @BindingAdapter("setNumberOfMinutes")
+        @JvmStatic
+        fun setNumberOfMinutes(textView: TextView, minutes: Int){
+            textView.text = minutes.toString()
+        }
+
+        @BindingAdapter("applyVeganColor")
+        fun applyVeganColor(view: View, vegan: Boolean){
+            if(vegan){
+                when(view){
+                    is TextView -> {
+                        view.setTextColor(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                    is ImageView ->{
+                        view.setColorFilter(
+                            ContextCompat.getColor(
+                            view.context,
+                            R.color.green
+                            )
+                        )
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/udemyfoodapp/ui/RecipesFragment.kt
+++ app/src/main/java/com/example/udemyfoodapp/ui/RecipesFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.udemyfoodapp.MainViewModel
 import com.example.udemyfoodapp.R
+import com.example.udemyfoodapp.adapters.RecipesAdapter
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
 
+    private lateinit var mainViewModel: MainViewModel
+    private val mAdapter by lazy { RecipesAdapter() }
+    private lateinit var mView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,12 +25,32 @@ class RecipesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
 
-        view.recyclerview.showShimmer()
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
 
-        return view
+        setupRecyclerView()
+
+        return mView
     }
 
+    private fun requestApiData(){
+        mainViewModel.getRecipes(queries: Map<String,String>)
 
+    }
+
+    private fun setupRecyclerView(){
+        mView.recyclerview.adapter = mAdapter
+        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect(){
+        mView.recyclerview.showShimmer()
+
+    }
+
+    private fun hideShimmerEffect(){
+        mView.recyclerview.hideShimmer()
+    }
 }
